7de18c30e632471bf222ca55f80ec8b2aa4f6227
nanayaw-kirk <nanayaw_@outlook.com>
2026-01-22 03:56:41 +0000

    Restore Website

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,6 +1,6 @@
 <template>
 	<div>
-		<!-- <ui-scroll-area class="h-dvh">
+		<ui-scroll-area class="h-dvh">
 			<div class="flex flex-col">
 				
 				<navs-header class="border-b"></navs-header>
@@ -9,21 +9,7 @@
 				</main>
 				<navs-footer class="py-12 md:py-20 pb-0"></navs-footer>
 			</div>
-		</ui-scroll-area> -->
-
-		<Head>
-			<Title>{{$appName}} Group of Companies - This is not a mistake!</Title>
-<!-- 			<Meta name="description" :content="`Group of Companies - Empowering Growth, Building Futures.`" />
-			<Meta name="keywords" :content="`Agriculture, Agric, Agro Company, Farm, Farmland, Farming, Mechanized Agriculture, Logistics, Last Mile Logistics, Construction, Construction Company, Real Estate`"></Meta>
-			<Meta name="robots" content="index, follow"></Meta> -->
-		</Head>
-
-
-		<div class="container p-0">
-			<div class="aspect-video">
-				<iframe class="w-full h-full !rounded-[inherit] !object-cover" src="https://www.youtube.com/embed/pJLk-IIjFek?list=RDpJLk-IIjFek&autoplay=1" title="Bad Meets Evil-Above The Law-Lyrics [HQ]" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-			</div>
-		</div>
+		</ui-scroll-area>
 
 	</div>
 
@@ -32,7 +18,6 @@
 
 <script setup lang="ts">
 import {useStorage} from "@vueuse/core"
-// const { authenticated, user } = storeToRefs(useAuthStore())
-const GLOBAL_PAGE_REFERESH = ref(false) //useStorage('GLOB_DID_ADM_REF', false)
+const GLOBAL_PAGE_REFERESH = ref(false)
 
 </script>
